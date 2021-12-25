f9b7061e94dcd31090656e483dc732e6e94a5d57
David Shams <david.shams.innova@gmail.com>
2021-12-24 23:36:47 -0600

    BUG: interfal refresh not working

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
 
 function Clock() {
+    // cancelInterval = setInterval(() => console.log(new Date()), 1000)
+
     let tick = () => {
         return (
             <h2>{new Date().toLocaleDateString()}</h2>
+
         )
     }
-    return (
-        <div>
-            {setInterval(tick, 1000)}
-        </div>
-    )
+    // return (
+    //     <div>
+    //         {/* {setInterval(tick, 1000)} */}
+    //         {
+
+    //             // setTimeout(() => clearInterval(cancelInterval), 5000)
+    //         }
+    //     </div>
+    // )
 }
 
 export default function Display() {
-   return (
-       setInterval(Clock, 1000)
-   )
-  }
-  
-  
+    return (
+        setInterval(Clock, 1000)
+    )
+}
+
