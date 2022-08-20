cbdb54504e91220a8bb70afd089c4368ab5f37b1
David Shams <david.shams.innova@gmail.com>
2022-08-20 14:57:39 -0500

    Comment buggy gatsby-images/

--- src/pages/gatsby-images.jsx
+++ src/pages/gatsby-images.jsx
@@ -1,28 +1,38 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+// import React from "react"
+// import { useStaticQuery, graphql } from "gatsby"
+// import Img from "gatsby-image"
 
-export default function Image() {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "../images/cat.jpeg" }) {
-        childImageSharp {
-          # Specify a fixed image and fragment.
-          # The default width is 400 pixels
-          fixed {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
+// export default function Image() {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       file(relativePath: { eq: "../images/cat.jpeg" }) {
+//         childImageSharp {
+//           # Specify a fixed image and fragment.
+//           # The default width is 400 pixels
+//           fixed {
+//             ...GatsbyImageSharpFixed
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   return (
+//     <div>
+//       <h1>Hello gatsby-image</h1>
+//       <Img
+//         fixed={data.file.childImageSharp.fixed}
+//         alt="Gatsby Docs are awesome"
+//       />
+//     </div>
+//   )
+// }
+
+import React from 'react'
+
+export default function GatsbyImages() {
   return (
-    <div>
-      <h1>Hello gatsby-image</h1>
-      <Img
-        fixed={data.file.childImageSharp.fixed}
-        alt="Gatsby Docs are awesome"
-      />
-    </div>
+    <>GatsbyImages
+    DEBUG ME
+    </>
   )
 }
